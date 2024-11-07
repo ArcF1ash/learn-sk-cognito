d42c25e57808b42849086f292091dd132f22e682
Aaron <aaron@emeryautomation.ca>
2024-11-06 16:55:12 -0800

    Added styles to UI

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
 
+    let { data } = $props();
+
     async function handleSignOut() {
         try {
             const response = await fetch('/api/auth/signout', { method: 'POST' });
@@ -13,6 +15,16 @@
     }
 </script>
 
-<h1>This is protected content. You are seeing this because you are signed in.</h1>
-
-<button on:click={handleSignOut}>Sign Out</button>
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="space-y-6 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div>
+            <h2 class="mt-10 text-center text-2xl/9 font-bold text-gray-900">Hello, { data.user.attributes.given_name } { data.user.attributes.family_name }!</h2>
+        </div>
+        <div>
+            <p class="text-center  text-gray-900">This is protected content. You are seeing this because you are signed in.</p>
+        </div>
+        <div>
+            <button onclick={handleSignOut} class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign Out</button>
+        </div>
+    </div>
+</div>
--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -7,6 +7,9 @@
     let confirmPassword: string = '';
     let authChallenge: string | null = null;
 
+    let showPassword: boolean = false;
+    $: type = showPassword ? 'text' : 'password';
+
     async function handleSubmit() {
         try {
             const response = await fetch('/api/auth/signin', {
@@ -62,30 +65,63 @@
     }
 </script>
 
-{#if authChallenge === 'NEW_PASSWORD_REQUIRED'}
-    <form on:submit|preventDefault={handleNewPasswordSubmit}>
-        <hgroup>
-            <h2>Set New Password</h2>
-        </hgroup>
-        <label for="username">New Password</label>
-        <input type="password" bind:value={newPassword} placeholder="New Password" required />
-
-        <label for="password">Confirm Password</label>
-        <input type="password" bind:value={confirmPassword} placeholder="Confirm New Password" required />
-
-        <button type="submit">Set Password</button>
-    </form>
-{:else}
-    <form on:submit|preventDefault={handleSubmit}>
-        <hgroup>
-            <h2>Sign In</h2>
-        </hgroup>
-        <label for="username">Email</label>
-        <input type="email" bind:value={email} placeholder="Email" required />
-
-        <label for="password">Password</label>
-        <input type="password" bind:value={password} placeholder="Password" required />
-
-        <button type="submit">Sign In</button>
-    </form>
-{/if}
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 class="mt-10 text-center text-2xl/9 font-bold text-gray-900">Sign in to your account</h2>
+    </div>
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        {#if authChallenge === 'NEW_PASSWORD_REQUIRED'}
+            <form class="space-y-6" on:submit|preventDefault={handleNewPasswordSubmit}>
+                <hgroup>
+                    <h3 class="mt-10 text-center font-bold text-gray-900">Set New Password</h3>
+                </hgroup>
+                <div>
+                    <label for="newPassword" class="block text-sm/6 font-medium text-gray-900">New Password</label>
+                    <div class="mt-2">
+                        <input id="newPassword" type={ type } bind:value={newPassword} required class="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                    </div>
+                </div>
+                <div>
+                    <label for="confirmPassword" class="block text-sm/6 font-medium text-gray-900">Confirm Password</label>
+                    <div class="mt-2">
+                        <input id="confirmPassword" type={ type } bind:value={confirmPassword} required class="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                    </div>
+                    <div class="mt-2 text-sm">
+                        <input id="showPasswords" type="checkbox" on:click="{ () => showPassword = !showPassword }">
+                        <label for="showPasswords">Show Password</label>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Set Password</button>
+                </div>
+            </form>
+        {:else}
+            <form class="space-y-6" on:submit|preventDefault={handleSubmit}>
+                <div>
+                    <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+                    <div class="mt-2">
+                        <input id="email" type="email" bind:value={email} required class="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                    </div>
+                </div>
+                <div>
+                    <div class="flex items-center justify-between">
+                      <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+                      <div class="text-sm">
+                        <a href="/" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                      </div>
+                    </div>
+                    <div class="mt-2">
+                      <input id="password" type={ type } bind:value={password} required class="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">
+                    </div>
+                    <div class="mt-2 text-sm">
+                        <input id="showPassword" type="checkbox" on:click="{ () => showPassword = !showPassword }">
+                        <label for="showPassword">Show Password</label>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                </div>
+            </form>
+        {/if}
+    </div>
+</div>
